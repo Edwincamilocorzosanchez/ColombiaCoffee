47b9ed44a907c34c4c0dc93e52cbdb3f0947b920
Edwin camilo corzo sanchez <194437744+Edwincamilocorzosanchez@users.noreply.github.com.>
2025-08-12 10:13:42 -0500

    feat: :zap: Creacion de clases

---
+++ Src/Modules/Users/Application/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ColombianCoffee.Src.Modules.Users.Application.Interfaces
+{
+    public interface IUserService
+    {
+        
+    }
+}
